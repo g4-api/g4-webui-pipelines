ae89c699b7430215c9571d82986457cefcb5cd24
Sabag <Roei.Sabag@finastra.com>
2025-03-02 11:24:28 +0200

    Url test

--- pipelines/python_code_pipeline.py
+++ pipelines/python_code_pipeline.py
@@ -1,6 +1,8 @@
 from typing import List, Union, Generator, Iterator
 import subprocess
 
+import requests
+
 
 class Pipeline:
     def __init__(self):
@@ -22,28 +24,33 @@ class Pipeline:
         print(f"on_shutdown:{__name__}")
         pass
 
-    def execute_python_code(self, code):
-        try:
-            result = subprocess.run(
-                ["python", "-c", code], capture_output=True, text=True, check=True
-            )
-            stdout = result.stdout.strip()
-            return stdout, result.returncode
-        except subprocess.CalledProcessError as e:
-            return e.output.strip(), e.returncode
-
     def pipe(
-        self, user_message: str, model_id: str, messages: List[dict], body: dict
-    ) -> Union[str, Generator, Iterator]:
+            self,
+            user_message: str,
+            model_id: str,
+            messages: List[dict],
+            body: dict) -> Union[str, Generator, Iterator]:
+
         # This is where you can add your custom pipelines like RAG.
         print(f"pipe:{__name__}")
-
-        print(messages)
         print(user_message)
+        print(model_id)
+        print(messages)
+        print(body)
 
-        if body.get("title", False):
-            print("Title Generation")
-            return "Python Code Pipeline"
-        else:
-            stdout, return_code = self.execute_python_code(user_message)
-            return stdout
+        url = "http://192.168.1.13:5000/ping"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+
+            # Parse JSON response into a Python dict
+            data = response.json()
+
+            # Extract the "message" field
+            message_value = data.get("message")
+
+            return message_value
+
+        except requests.exceptions.RequestException as e:
+            return f"{e}"
